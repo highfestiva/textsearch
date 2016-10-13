079364b77fb8779a8548dc10dcf6abb628aa8cd0
Jonas Bystrom <jonas.bystrom@hm.com>
2016-10-13 15:20:13 +0200

    Added support for both case-sensitive and case-insensitive searches.

--- textsearch.py
+++ textsearch.py
@@ -8,10 +8,15 @@ excludedirs = set('.git .svn __pycache__'.split())
 excludeexts = set('.exe .bin .png .jpg .gif .tga .ppm .ico .icns .psd .wav .mp3 .ttf .afm .eot .zip .7z .gz .jar .pyc .pyo .pyd .whl .class .war .dll .obj .pch .pdb .ilk .suo'.split())
 maxfilesize = 1024*1024
 
-tostr = lambda b: b.decode('utf8','ignore')
+case_insensitive,argv = '--case-insensitive' in argv, [a for a in argv if a!='--case-insensitive']
 
-search = ' '.join(argv[1:]).encode()
-print('Text searching for "%s"...' % tostr(search))
+tosearch = (lambda a: a.lower()) if case_insensitive else (lambda a: a.encode())
+todata = (lambda a: a.decode('utf8').lower()) if case_insensitive else (lambda a: a)
+toprint = (lambda a: a) if case_insensitive else (lambda a: a.decode('utf8','ignore'))
+bin2string = lambda b: b.__str__()[2:-1]
+
+search = tosearch(' '.join(argv[1:]))
+print('Text searching for "%a"...' % toprint(search))
 i = 0
 def walkdir(root):
 	global i
@@ -29,12 +34,17 @@ def walkdir(root):
 			i += 1
 			with open(f, 'rb') as r:
 				data = r.read()
+				try:
+					data = todata(data)
+				except UnicodeDecodeError:
+					# Fast conversion of binary or non-utf8 file.
+					data = bin2string(data)
 				while True:
 					index = data.index(search)
-					found = ' '.join(tostr(data[index-10:index+len(search)+20]).split())
+					found = ' '.join(toprint(data[index-10:index+len(search)+20]).split())
 					print('%s: %s' % (f, found))
 					data = data[index+1:]
-		except:
+		except Exception as e:
 			pass
 	for d in dirs:
 		walkdir(d)
