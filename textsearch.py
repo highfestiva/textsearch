bb03d5b0e59644d085a2887f094696491deb4200
Jonas B <jonas.bystrom@hm.com>
2016-10-13 13:52:43 +0200

    Had to make a minor change for a magnitude speedup.

--- textsearch.py
+++ textsearch.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-from codecs import open
 from os import listdir,stat
 from os.path import join as pathjoin,isfile,isdir,splitext
 from sys import argv,stderr
@@ -9,8 +8,10 @@ excludedirs = set('.git .svn __pycache__'.split())
 excludeexts = set('.exe .bin .png .jpg .gif .tga .ppm .ico .icns .psd .wav .mp3 .ttf .afm .eot .zip .7z .gz .jar .pyc .pyo .pyd .whl .class .war .dll .obj .pch .pdb .ilk .suo'.split())
 maxfilesize = 1024*1024
 
-search = ' '.join(argv[1:])
-print('Text searching for "%s"...' % search)
+tostr = lambda b: b.decode('ascii','ignore')
+
+search = ' '.join(argv[1:]).encode()
+print('Text searching for "%s"...' % tostr(search))
 i = 0
 def walkdir(root):
 	global i
@@ -23,14 +24,14 @@ def walkdir(root):
 		if stat(f).st_size > maxfilesize:
 			continue
 		try:
-			if i%877:
+			if not i&0x3ff:
 				print(f[:79].ljust(79),end='\r',file=stderr)
 			i += 1
-			with open(f, encoding='ascii', errors='ignore') as r:
+			with open(f, 'rb') as r:
 				data = r.read()
 				while True:
 					index = data.index(search)
-					found = ' '.join(data[index-10:index+len(search)+20].split())
+					found = ' '.join(tostr(data[index-10:index+len(search)+20]).split())
 					print('%s: %s' % (f, found))
 					data = data[index+1:]
 		except:
